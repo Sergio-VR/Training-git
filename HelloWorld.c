b4959d6f82868844a506d7b1320164e3627bcadd
Sergio-VR <74207251+Sergio-VR@users.noreply.github.com>
2021-02-23 17:48:22 +0100

    Bucle y operaciones

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,9 +2,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define lim 10
+
 int main()
 {
+	uint32_t a,b,c;
+	uint16_t u,i;
+	i=0;
+	u = 12;
+	
+	for (i = 0; i<=lim; i++)
+	{
 
-	printf ("Hello World");
+		
+		a = a + u--;
+		b = b + u++;
+		c = a - b;
+		
+		printf("a tiene el valor: %u \r\n", a); 
+		printf("b tiene el valor: %u \r\n", b); 
+		printf("c tiene el valor: %u \r\n", c); 
+	}
 
+	return 0;
 } 
